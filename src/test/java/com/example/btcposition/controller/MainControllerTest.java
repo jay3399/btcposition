5e59f048e0491b564ffe44be0d50c5bb6388ca91
jay <joswxx@gmail.com>
2023-08-16 20:42:45 +0900

    =

--- src/test/java/com/example/btcposition/controller/MainControllerTest.java
+++ src/test/java/com/example/btcposition/controller/MainControllerTest.java
@@ -1,76 +1,76 @@
-package com.example.btcposition.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.example.btcposition.JwtTokenUtil;
-import com.example.btcposition.reposiotry.VoteRepository;
-import com.example.btcposition.service.RedisService;
-import com.example.btcposition.service.VoteService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-
-@SpringBootTest
-@AutoConfigureMockMvc
-class MainControllerTest {
-
-  @Autowired
-  private MockMvc mockMvc;
-
-  @MockBean
-  private VoteService voteService;
-
-  @MockBean
-  private RedisService redisService;
-
-
-//  @BeforeEach
-//  public void setUp() {
-//    doNothing().when(redisService).isExist(null);
+//package com.example.btcposition.controller;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.ArgumentMatchers.anyString;
+//import static org.mockito.Mockito.doNothing;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.when;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import com.example.btcposition.JwtTokenUtil;
+//import com.example.btcposition.reposiotry.VoteRepository;
+//import com.example.btcposition.service.RedisService;
+//import com.example.btcposition.service.VoteService;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mock;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//class MainControllerTest {
+//
+//  @Autowired
+//  private MockMvc mockMvc;
+//
+//  @MockBean
+//  private VoteService voteService;
+//
+//  @MockBean
+//  private RedisService redisService;
+//
+//
+////  @BeforeEach
+////  public void setUp() {
+////    doNothing().when(redisService).isExist(null);
+////  }
+//
+//
+//  @Test
+//  public void testVoteWithoutToken() throws Exception {
+//    // JWT 토큰이 없을 때의 테스트
+//    mockMvc.perform(post("/vote")
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .param("value", "long"))
+//                    .andExpect(redirectedUrl("/token"));
 //  }
-
-
-  @Test
-  public void testVoteWithoutToken() throws Exception {
-    // JWT 토큰이 없을 때의 테스트
-    mockMvc.perform(post("/vote")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .param("value", "long"))
-                    .andExpect(redirectedUrl("/token"));
-  }
-
-
-  @Test
-  void getResults() {
-  }
-
-  @Test
-  void generateToken() {
-  }
-
-  @Test
-  void isVoted() {
-  }
-
-  @Test
-  void synRedisWithMysql() {
-  }
-}
+//
+//
+//  @Test
+//  void getResults() {
+//  }
+//
+//  @Test
+//  void generateToken() {
+//  }
+//
+//  @Test
+//  void isVoted() {
+//  }
+//
+//  @Test
+//  void synRedisWithMysql() {
+//  }
+//}
--- src/test/java/com/example/btcposition/service/VoteServiceTest.java
+++ src/test/java/com/example/btcposition/service/VoteServiceTest.java
@@ -1,115 +1,115 @@
-package com.example.btcposition.service;
-
-import static com.example.btcposition.domain.VoteType.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-import com.example.btcposition.domain.Vote;
-import com.example.btcposition.domain.VoteSummary;
-import com.example.btcposition.domain.VoteType;
-import com.example.btcposition.reposiotry.VoteSummaryRepository;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import com.example.btcposition.reposiotry.VoteRepository;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.mockito.Mockito.*;
-
-@SpringBootTest
-class VoteServiceTest {
-
-    @MockBean
-    private VoteRepository voteRepository;
-
-    @Autowired
-    private VoteSummaryRepository repository;
-
-    @Autowired
-    private VoteService voteService;
-
-
-//    @BeforeEach
-//    public void setUp() {
-////        voteRepository = mock(VoteRepository.class);
-//        voteService = new VoteService(voteRepository);
+//package com.example.btcposition.service;
+//
+//import static com.example.btcposition.domain.VoteType.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import com.example.btcposition.domain.Vote;
+//import com.example.btcposition.domain.VoteSummary;
+//import com.example.btcposition.domain.VoteType;
+//import com.example.btcposition.reposiotry.VoteSummaryRepository;
+//import java.time.LocalDate;
+//import java.time.format.DateTimeFormatter;
+//import java.util.ArrayList;
+//import java.util.List;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import com.example.btcposition.reposiotry.VoteRepository;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static org.mockito.Mockito.*;
+//
+//@SpringBootTest
+//class VoteServiceTest {
+//
+//    @MockBean
+//    private VoteRepository voteRepository;
+//
+//    @Autowired
+//    private VoteSummaryRepository repository;
+//
+//    @Autowired
+//    private VoteService voteService;
+//
+//
+////    @BeforeEach
+////    public void setUp() {
+//////        voteRepository = mock(VoteRepository.class);
+////        voteService = new VoteService(voteRepository);
+////    }
+//
+//
+//
+//
+//    @Test
+//    void getVote() {
+//
+//        String value = "value";
+//
+//        Vote expectedVote = mock(Vote.class);
+//
+//        when(voteRepository.findByValue(LONG)).thenReturn(expectedVote);
+//
+//
+//        Vote result = voteService.getVote(LONG);
+//
+//        assertEquals(expectedVote, result);
+//        verify(voteRepository).findByValue(LONG);
+//
+//
 //    }
-
-
-
-
-    @Test
-    void getVote() {
-
-        String value = "value";
-
-        Vote expectedVote = mock(Vote.class);
-
-        when(voteRepository.findByValue(LONG)).thenReturn(expectedVote);
-
-
-        Vote result = voteService.getVote(LONG);
-
-        assertEquals(expectedVote, result);
-        verify(voteRepository).findByValue(LONG);
-
-
-    }
-
-
-
-
-    @Test
-    @Transactional
-    void saveVote() {
-        Vote vote = mock(Vote.class);
-
-        voteService.saveVote(vote);
-
-        verify(voteRepository).save(vote);
-
-    }
-
-    @Test
-    @Transactional
-    void updateVote() {
-
-        List<Vote> votes = new ArrayList<>();
-
-        Vote vote = mock(Vote.class);
-
-        vote.setValue(LONG);
-        vote.setCount(1);
-        votes.add(vote);
-
-        voteService.updateVote(votes);
-
-        verify(voteRepository).updateVoteCount(vote.getValue(), vote.getCount());
-
-
-    }
-
-    @Test
-    void findAll() {
-
-        List<Vote> votes = new ArrayList<>();
-
-        when(voteRepository.findAll()).thenReturn(votes);
-
-        List<Vote> foundVotes = voteService.findAll();
-
-        assertEquals(votes, foundVotes);
-        verify(voteRepository).findAll();
-
-
-    }
-}
+//
+//
+//
+//
+//    @Test
+//    @Transactional
+//    void saveVote() {
+//        Vote vote = mock(Vote.class);
+//
+//        voteService.saveVote(vote);
+//
+//        verify(voteRepository).save(vote);
+//
+//    }
+//
+//    @Test
+//    @Transactional
+//    void updateVote() {
+//
+//        List<Vote> votes = new ArrayList<>();
+//
+//        Vote vote = mock(Vote.class);
+//
+//        vote.setValue(LONG);
+//        vote.setCount(1);
+//        votes.add(vote);
+//
+//        voteService.updateVote(votes);
+//
+//        verify(voteRepository).updateVoteCount(vote.getValue(), vote.getCount());
+//
+//
+//    }
+//
+//    @Test
+//    void findAll() {
+//
+//        List<Vote> votes = new ArrayList<>();
+//
+//        when(voteRepository.findAll()).thenReturn(votes);
+//
+//        List<Vote> foundVotes = voteService.findAll();
+//
+//        assertEquals(votes, foundVotes);
+//        verify(voteRepository).findAll();
+//
+//
+//    }
+//}
